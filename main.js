5388cf4a196501d7bc68a92d7bf4a5aa93f8a924
Adnan Zawad Toky <adnanzawad@yahoo.com>
2019-01-09 21:39:58 +0500

    Added comments

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// Gets window height & width
 var w = window.innerWidth;
 var h = window.innerHeight;
+// Makes canvas height & width equal
 if(w > h){
     w = h-100;
     h = w;
@@ -10,18 +12,20 @@ if(w < 100){
 }
 h = w;
 
-var x = w/2;
-var y = w/2;
-var r = 2;
-var number = 500;
-var v = 3;
-var angle = Math.PI*2/number;
-var balls = [];
-var red = 255;
-var green = 255;
-var blue = 255;
-var intv;
+var x = w/2; // Center-X of canvas
+var y = w/2; // Center-Y of canvas
+var r = 2; // Radius of balls
+var number = 500; // Number of balls
+var v = 3; // Velocity of balls
+var angle = Math.PI*2/number; // Angle between two ball's path
+var balls = []; // Ball's container
+var red = 255; // Color red
+var green = 255; // Color green
+var blue = 255; // Color blue
+var intv; // Interval
 
+// Changes the color style
+// dr -> red color's changing rate, dg -> green color's changing rate, db -> blue color's changing rate
 function colorChange(dr,dg,db){
     red -= dr;
     if(red < 10){
@@ -39,42 +43,46 @@ function colorChange(dr,dg,db){
     }
 }
 
+// Prepares ball's data
 function ready(){
     balls = [];
     for(n = 0;n < number;n++){
         balls[n] = {
-            bx:x,
-            by:y,
-            dx:v*Math.sin(n*angle),
-            dy:v*Math.cos(n*angle)
+            bx:x, // Ball's position-X
+            by:y, // Ball's position-Y
+            dx:v*Math.sin(n*angle), // Changing rate of bx
+            dy:v*Math.cos(n*angle) // Changing rate of by
         }
     }
 }
 
-
+// Gets canvas element
 var c = document.getElementById("myCanvas");
-    c.height = w;
-    c.width = w;
-var ctx = c.getContext("2d");
+// Sets canvas height & width
+c.height = w;
+c.width = w;
+var ctx = c.getContext("2d"); // Gets canvas context
 
+// Draws balls
 function draw(){
     for(n = 0;n < number;n++){
         ctx.beginPath();
-        ctx.arc(balls[n].bx+balls[n].dx,h-(balls[n].by+balls[n].dy),r,0,Math.PI*2);
-        ctx.fillStyle = "rgb("+red+","+green+","+blue+")";
+        ctx.arc(balls[n].bx+balls[n].dx,h-(balls[n].by+balls[n].dy),r,0,Math.PI*2); // Draws round ball
+        ctx.fillStyle = "rgb("+red+","+green+","+blue+")"; // Sets ball's color
         ctx.fill();
         ctx.closePath();
-        balls[n].bx += balls[n].dx;
-        balls[n].by += balls[n].dy;
+        balls[n].bx += balls[n].dx; // Makes change to ball's x position
+        balls[n].by += balls[n].dy; // Makes change to ball's y position
         if(balls[n].by < 0 || balls[n].by > h){
-            balls[n].dy =- balls[n].dy;
+            balls[n].dy =- balls[n].dy; // Changes ball's direction if reaches at top or bottom
         }
         if(balls[n].bx < 0 || balls[n].bx > w){
-            balls[n].dx =- balls[n].dx;
+            balls[n].dx =- balls[n].dx; // Changes ball's direction if reaches at left or right
         }
     }
 }
 
+// Updates animation data by user input
 function update(drc,dgc,dbc){
     number=document.getElementById("number").value;
     angle=Math.PI*2/number;
@@ -92,6 +100,7 @@ function update(drc,dgc,dbc){
     },10)
 }
 
+// Initializes animation after loading the page
 window.onload = function(){
     setTimeout(function(){
         update(10,30,40);
